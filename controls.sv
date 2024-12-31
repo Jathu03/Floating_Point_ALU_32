e8e05d57c2a01673a27ba11f45f4a453fd1f6fb6
Jathu03 <129855031+Jathu03@users.noreply.github.com>
2024-12-31 23:09:37 +0530

    Update controls.sv

--- controls.sv
+++ controls.sv
@@ -2,19 +2,19 @@
 `define CONTROLS_SV_INCLUDED
 
 // ALU control signals
-`define ALU_ADD 'b0000
-`define ALU_SUB 'b0001
-`define ALU_SLL 'b0010
-`define ALU_SRL 'b0011
-`define ALU_SRA 'b0100
-`define ALU_AND 'b0101
-`define ALU_OR  'b0110
-`define ALU_XOR 'b0111
-`define ALU_SLT 'b1000
-`define ALU_SLTU 'b1001
-`define ALU_A   'b1010
-`define ALU_B   'b1011
-`define ALU_MUL 'b1100
+`define ADD 'b0000
+`define SUB 'b0001
+`define SLL 'b0010
+`define SRL 'b0011
+`define SRA 'b0100
+`define AND 'b0101
+`define OR  'b0110
+`define XOR 'b0111
+`define SLT 'b1000
+`define SLTU 'b1001
+`define A   'b1010
+`define B   'b1011
+`define MUL 'b1100
 
 // Opcode types
 `define TYPE_R 	  7'b0110011
